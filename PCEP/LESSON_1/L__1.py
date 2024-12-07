033ea11d9e3b3445e54243c5129e0dd41ffb7fe3
shalini-tiwari09 <shalinitiwari.jetlearn@gmail.com>
2024-12-07 15:54:59 +0100

    Update L__1.py

--- PCEP/LESSON_1/L__1.py
+++ PCEP/LESSON_1/L__1.py
@@ -2,34 +2,9 @@ age = 30
 height = 5.4
 name = "Shalini"
 is_student = False
-
+is_teacher = True
 print("Age", age, "Data Type", type(age))
 print("name", name, "Data Type", type(name))
 print("height", height, "Data Type", type(height))
 print("Is a Student", is_student, "Data Type", type(is_student))
-
-
-#Condition Check
-surname = input("enter title")
-
-if surname:
-    print(surname)
-else:
-    print("Invalid what is this")
-    
-if age:
-    print("Valid age")
-else :
-    print("Invalid")        
-    
-if is_student:
-    print("Valid")
-else:
-    print("Invalid")        
-    
-is_teacher = True
-
-if is_teacher:
-    print("Valid")
-else:
-    print("Invalid")            
+print("Is a Teacher", is_teacher, "Data Type", type(is_teacher))
